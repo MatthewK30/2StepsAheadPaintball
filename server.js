acf9c443acf1b0af20232537491a39a87cdd8be7
MatthewK30 <278221103+MatthewK30@users.noreply.github.com>
2026-05-03 15:30:07 -0700

    Add Beach Mansion Mediterranean cliff side map

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ function normalizeTeam(team) {
 }
 
 function normalizeMap(map) {
-  return map === 'funland' || map === 'ghosttown' || map === 'rooftop' || map === 'castle' ? map : 'speedball'
+  return map === 'funland' || map === 'ghosttown' || map === 'rooftop' || map === 'castle' || map === 'beachmansion' ? map : 'speedball'
 }
 
 function normalizeGameMode(gameMode) {
@@ -157,7 +157,7 @@ function normalizeSkin({ skinId, skinFile, skinName } = {}) {
 }
 
 function isValidMap(map) {
-  return map === 'speedball' || map === 'funland' || map === 'ghosttown' || map === 'rooftop' || map === 'castle'
+  return map === 'speedball' || map === 'funland' || map === 'ghosttown' || map === 'rooftop' || map === 'castle' || map === 'beachmansion'
 }
 
 function isValidGameMode(gameMode) {
